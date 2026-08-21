a2618fcf685e4026208b1f974476d995946f6eaf
geodepartement18 <114421205+geodepartement18@users.noreply.github.com>
2026-08-21 17:08:35 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 
+import Portal from "https://js.arcgis.com/4.34/@arcgis/core/portal/Portal.js";
+import FeatureLayer from "https://js.arcgis.com/4.34/@arcgis/core/layers/FeatureLayer.js";
+import Map from "https://js.arcgis.com/4.34/@arcgis/core/Map.js";
+import MapView from "https://js.arcgis.com/4.34/@arcgis/core/views/MapView.js";
+import { CONFIG } from "./config.js";
+
+// Function to query for portal items.
+      function queryItems(portal) {
+        // Create query parameters for the portal search.
+        const queryParams = new PortalQueryParams({
+          query: "group:" + ${CONFIG.groupId},
+          sortField: "num-views",
+          sortOrder: "desc",
+          num: 20,          
+        });
+        // Query the items based on the queryParams created from the portal.
+        portal.queryItems(queryParams).then(createGallery);
+      }
+
+async function load() {
+    portal=new Portal({url:CONFIG.portalUrl});
+    portal.authMode="anonymous";
+    await portal.load();
+
+    queryItems();
+}
+
+load();
