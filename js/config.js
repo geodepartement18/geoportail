0707d60de39a7dfc0e9a7b304f79b76ed423b4e4
geodepartement18 <114421205+geodepartement18@users.noreply.github.com>
2026-08-21 17:09:13 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,24 @@
+export const CONFIG = {
+  portalUrl: "https://cher.maps.arcgis.com/",
+  groupId: "a249c493cd4d44c39c6565d996cb83d6",
+
+  siteTitle: "Portail Open Data",
+  siteDescription: "Catalogue de données publiques",
+
+  maxItems: 10,
+  pageSize: 12,
+
+  allowedTypes: [
+    "Feature Service",
+    "Map Service",
+    "CSV",
+    "Shapefile",
+    "GeoJson",
+    "KML",
+    "WFS",
+    "WMS",
+    "Vector Tile Service",
+    "Image Service",
+    "File Geodatabase"
+  ]
+};
