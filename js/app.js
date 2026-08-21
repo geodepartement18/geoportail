9a1fc7134c19d9b3974960efeafcde3133c80e5a
geodepartement18 <114421205+geodepartement18@users.noreply.github.com>
2026-08-21 18:36:09 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import MapView from "https://js.arcgis.com/4.34/@arcgis/core/views/MapView.js";
 import { CONFIG } from "./config.js";
 
 // Function to query for portal items.
-    function queryItems() {
+    function queryItems(portal) {
         // Create query parameters for the portal search.
         const queryParams = new portal.queryItems({
           query: `group:${CONFIG.groupId}`,
@@ -19,7 +19,7 @@ import { CONFIG } from "./config.js";
     }
 
 async function load() {
-    var portal=new Portal({url:CONFIG.portalUrl});
+    var portal = new Portal({url:CONFIG.portalUrl});
     portal.authMode="anonymous";
     await portal.load();
 
