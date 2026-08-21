c25d35c0b4f43c6e04e328b11e19550c07527370
geodepartement18 <114421205+geodepartement18@users.noreply.github.com>
2026-08-21 22:42:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import MapView from "https://js.arcgis.com/4.34/@arcgis/core/views/MapView.js";
 import { CONFIG } from "./config.js";
 
 // Function to query for portal items.
-    function listItems(portal) {
+function listItems(portal) {
         // Create query parameters for the portal search.
         const queryParams = new PortalQueryParams({
           query: `group:${CONFIG.groupId}`,
@@ -15,8 +15,41 @@ import { CONFIG } from "./config.js";
           num: 20,          
         });
         // Query the items based on the queryParams created from the portal.
-        portal.queryItems(queryParams).then(openAlert);
-    }
+        portal.queryItems(queryParams).then(createGallery);
+}
+
+ // Function to build the UI for the gallery to display queried portal items.
+      function createGallery(items) {
+        items.results.forEach((item) => {
+          // Create a card for each item and add a thumbnail, title, subtitle,
+          // view count value, and a button to open the item in a new window.
+          const card = document.createElement("calcite-card");
+          const thumbnail = document.createElement("img");
+          thumbnail.slot = "thumbnail";
+          thumbnail.src = item.thumbnailUrl;
+
+          const title = document.createElement("span");
+          title.slot = "heading";
+          title.style = "overflow: hidden; white-space: nowrap; text-overflow: ellipsis;";
+          title.textContent = item.title;
+
+          const type = document.createElement("span");
+          type.slot = "description";
+          type.textContent = item.type;
+
+          const views = document.createElement("span");
+          views.slot = "footer-end";
+          views.textContent = "Views: " + item.numViews;
+
+          const openItemAction = document.createElement("calcite-action");
+          openItemAction.icon = "launch";
+          openItemAction.slot = "footer-end";
+          openItemAction.value = item;
+          // Add event listener to open the item details page in a new window.
+          openItemAction.addEventListener("click", (event) => {
+            window.open(event.target.value.itemPageUrl);
+          });
+
 function openAlert(){
     alert("liste des items ok !");
 }
@@ -27,5 +60,3 @@ async function load() {
 }
 
 load();
-
-
